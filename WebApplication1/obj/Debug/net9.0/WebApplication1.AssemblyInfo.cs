11a588bf848da3227d3c4c8d08d4caaf4c556ab0
Turliu Cezar <cezarturliu25@gmail.com>
2025-07-28 15:50:16 +0300

    removed global

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f7fd3c963879fb37ed6d50a06acdad2bca700d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76ff448c0f25a595606a4f1892b22e46e95c2dfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
