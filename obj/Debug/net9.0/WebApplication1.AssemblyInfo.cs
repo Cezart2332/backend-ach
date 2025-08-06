64ad2de0546288aa3bd8efc99536aa78538e21d2
Turliu Cezar <cezarturliu25@gmail.com>
2025-08-06 21:57:12 +0300

    changed events logic

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e518f566d1f3b6cf51a3e5d2d7b943471936653")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3a4e1c4553c1950bf786e2de05322f0282502d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
