2daaff46f4bce88118c2b452d2ea3903f09ea3af
Your Name <you@example.com>
2025-08-25 16:53:26 +0300

    /\,

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64dd8f294b161efd654be230898e5dc1653ad1dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d1af406c48e1f85ebc069c118ae9f207c772af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
